2364d28e9f730343d3f7ae8ad7151c3c15a2cc84
Eeshu Narayan <eeshunarayan15@gmail.com>
2025-01-11 13:56:33 +0530

    updating the cutomer table in the sql file

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -7,3 +7,16 @@ INSERT IGNORE INTO users (username, password, enabled) VALUES ('user', '{noop}ee
 INSERT IGNORE INTO authorities (username, authority) VALUES ('user', 'read');
 INSERT IGNORE INTO users (username, password, enabled) VALUES ('user', '{bcrypt}$2a$12$R1.9hU81Jc4SEV5FTK36Yea.UY5YFAACai0qZWEX5YGvee5DyMASe', '1');
 INSERT IGNORE INTO authorities (username, authority) VALUES ('admin', 'admin');
+====================================================================
+
+CREATE TABLE customer(id int NOT NULL AUTO_INCREMENT,
+email varchar(45) NOT NULL,
+pwd varchar(250) NOT NULL,
+role varchar(45) NOT NULL,
+PRIMARY KEY(id)
+);
+INSERT INTO customer(email,pwd,role)VALUES("eeshunarayan15@gmail.com","{noop}eeshu","read");
+INSERT INTO customer(email,pwd,role)VALUES("eeshunarayan15@gmail.com","{bcrypt}$2a$12$R1.9hU81Jc4SEV5FTK36Yea.UY5YFAACai0qZWEX5YGvee5DyMASe","read");
+UPDATE customer 
+SET email = "eeshunarayan16@gmail.com" 
+WHERE email = "eeshunarayan15@gmail.com";
